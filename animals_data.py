0fd6e248d014d495bc9d7e714bbe52a0be4455e6
Anuoluwapokudirat-cyber <anuoluwalokudirat@gmail.com>
2026-09-09 14:34:19 +0100

    Add new animals to animals_data.py

--- animals_data.py
+++ animals_data.py
@@ -11,10 +11,31 @@ ANIMALS = {
         "name_kh": "ផ្សោត (Dolphin)",
         "description": "ថនិកសត្វក្នុងទឹក ឆ្លាតវៃ ចេះលោត និងមានសមត្ថភាពទំនាក់ទំនងល្អ",
         "image_path": "static/dolphin.jpg"
+    },
+    "swallow": {
+        "name_kh": "ត្រចៀកកោង (Swallow)",
+        "description": "សត្វស្លាបតូច ហើរលឿន ចេះធ្វើសំបុកនៅលើផ្ទះ",
+        "image_path": "static/swallow.jpg"
+    },
+    "whale": {
+        "name_kh": "ត្រីបាឡែន (Whale)",
+        "description": "ថនិកសត្វក្នុងទឹកដ៏ធំ អាចហែលបានចម្ងាយឆ្ងាយ",
+        "image_path": "static/whale.jpg"
+    },
+    "seagull": {
+        "name_kh": "ត្រីសមុទ្រ (Seagull)",
+        "description": "សត្វស្លាបសមុទ្រ ហើរបានឆ្ងាយ និងអាចហែលទឹកបាន",
+        "image_path": "static/seagull.jpg"
+    },
+    "penguin": {
+        "name_kh": "ភេនឃ្វីន (Penguin)",
+        "description": "សត្វស្លាបមិនអាចហើរបាន ប៉ុន្តែហែលទឹកបានយ៉ាងពូកែ",
+        "image_path": "static/penguin.jpg"
     }
 }
 
 def get_daily_content():
+    """Get today's animal based on day of year."""
     day_of_year = datetime.datetime.now().timetuple().tm_yday
     animal_keys = list(ANIMALS.keys())
     index = (day_of_year - 1) % len(animal_keys)
